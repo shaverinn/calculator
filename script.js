543dbd77d784a0803790119a4835387282080ff3
Илья <shaverin65@mail.ru>
2024-02-24 10:43:19 +0300

    22

---
+++ script.js
@@ -0,0 +1,23 @@
+const buttons = document.querySelector('.buttons')
+const tablo = document.querySelector('.tablo')
+
+const numbers = ['0','1','2','3','4','5','6','7','8','9']
+const operators = ['AC','+/-','%','/','*','-','+','=',',']
+
+let num1 = 0
+let num2 = 0 
+let operator = ''
+
+buttons.addEventListener('click', (Event)=>{
+    const context = Event.target.innerText
+
+    if(numbers.includes(context)){
+        console.log('Число')
+        tablo.value = context
+        num1 = +tablo.value
+    } else if (operators.includes(context)){
+        console.log('оператор')
+    }else {
+        console.log ('не оператор и не число')
+    }
+})
